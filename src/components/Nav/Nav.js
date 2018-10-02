978ce243919f680b57a125771c01d6f782bdbfe3
Stephen Ridings <SRidings777@aol.com>
2018-10-02 05:43:40 -0500

    Updated link
    Updated link

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ const Nav = props => (
   <nav>
     <ul>
       <li className="brand animated lightSpeedIn">
-        <a href="/Clicky-Game/">Street Fighter Clicky Game</a>
+        <a href="/sfv-app/">Street Fighter Clicky Game</a>
       </li>
 
       <li id="rw">{props.rightWrong}</li>
