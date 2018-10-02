7cbd6a5e8ab6698809685638658cba4124f1946d
Stephen Ridings <sridings421@gmail.com>
2018-10-02 03:07:03 -0500

    test with src

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = props => (
+  <nav>
+    <ul>
+      <li className="brand animated lightSpeedIn">
+        <a href="/Clicky-Game/">Street Fighter Clicky Game</a>
+      </li>
+
+      <li id="rw">{props.rightWrong}</li>
+
+
+
+      <li id="cur-sco">Current Score: {props.score}</li>
+
+      <li id="top-sco">Top Score: {props.topScore}</li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
